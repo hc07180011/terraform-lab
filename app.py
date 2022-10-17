25929e1379629743d38161c8504dc2c2d5f0a2ac
Henry Chao <henryxyz@Henrys-MacBook-Pro.local>
2022-10-17 14:27:26 +0800

    update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ logger.setLevel(logging.INFO)
 
 
 def handler1(event, context):
-    return {"status": "`handler1-v2` triggered"}
+    return {"status": "`handler1-v3` triggered"}
 
 
 def handler2(event, context):
-    return {"status": "`handler2-v2` triggered"}
+    return {"status": "`handler2-v3` triggered"}
