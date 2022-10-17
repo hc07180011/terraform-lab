c2ba25159c23bb2d307f7d3b9ea4976edda01f6e
Henry Chao <henryxyz@Henrys-MacBook-Pro.local>
2022-10-17 17:23:55 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 import logging
 
+from utils.logger import init_logger
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+
+logger = init_logger()
 
 
 def handler1(event, context):
-    return {"status": "`handler1-v3` triggered"}
+    logger = logging.getLogger("rich")
+    logger.info("`handle1` triggerd")
+    return {"status": "`handler1-v4` triggered"}
 
 
 def handler2(event, context):
-    return {"status": "`handler2-v3` triggered"}
+    logger = logging.getLogger("rich")
+    logger.info("`handle2` triggerd")
+    return {"status": "`handler2-v4` triggered"}
+
+
+def f3():
+    print(1 / 0)
+
+
+def f2():
+    f3()
+
+
+def f1():
+    f2()
+
+
+def handler3(event, context):
+    logger = logging.getLogger("rich")
+    logger.info("`handle3` triggerd")
+    try:
+        f1()
+        return {"status": "`handler3-v4` triggered"}
+    except Exception as e:
+        logger.critical(e, exc_info=True)
+        return {"status": "internal error", "message": repr(e)}
