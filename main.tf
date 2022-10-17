29667e985bfdc9cf30e5196571f3f00de3df0b9c
Henry Chao <henryxyz@Henrys-MacBook-Pro.local>
2022-10-17 14:10:39 +0800

    update tf

--- main.tf
+++ main.tf
@@ -24,30 +24,6 @@ resource "aws_ecr_repository" "hello_world_repo" {
   name = "hello-world"
 }
 
-resource "aws_ecr_repository_policy" "hello_world_repo_policy" {
-  repository = aws_ecr_repository.hello_world_repo.name
-
-  policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "LambdaECRImageRetrievalPolicy",
-            "Effect": "Allow",
-            "Principal": "lambda.amazonaws.com",
-            "Action": [
-                "ecr:BatchGetImage",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy",
-                "ecr:GetRepositoryPolicy"
-            ]
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_ecr_lifecycle_policy" "hello_world_lifecycle_policy" {
   repository = aws_ecr_repository.hello_world_repo.name
 
